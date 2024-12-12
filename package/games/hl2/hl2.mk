66bd6d6edbb4a3b03723a3d2fc25e370adb3d0fc
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-12-11 22:10:16 -0500

    Update Half-Life 2

--- package/games/hl2/hl2.mk
+++ package/games/hl2/hl2.mk
@@ -3,12 +3,12 @@
 # source-engine configured for hl2
 #
 ################################################################################
-# Version: Commits on Aug 23, 2023
-HL2_VERSION = b7bd94c52ea7c4809e01b5af5bc14ab7061862a2
+# Version: Commits on Dec 30, 2023
+HL2_VERSION = 29985681a18508e78dc79ad863952f830be237b6
 HL2_SITE = https://github.com/nillerusr/source-engine.git
 HL2_SITE_METHOD = git
 HL2_GIT_SUBMODULES = yes
-HL2_CONF_OPTS = -T release --build-games=hl2 --prefix=/usr/bin/hl2
+HL2_CONF_OPTS = -T release --build-games=hl2 --prefix=/usr/bin/games/hl2
 HL2_NEEDS_EXTERNAL_WAF = NO
 HL2_LICENSE = SOURCE1SDK
 HL2_LICENSE_FILE = LICENSE
@@ -19,12 +19,11 @@ ifeq ($(BR2_PACKAGE_GL4ES),y)
 endif
 
 ifeq ($(BR2_aarch64),y)
-	HL2_CONF_OPTS += --64bits
     HL2_ARCH = arm64
 else ifeq ($(BR2_arm),y)
+    HL2_CONF_OPTS += --32bits
     HL2_ARCH = armv7l
 else ifeq ($(BR2_x86_64),y)
-    HL2_CONF_OPTS += --64bits
     HL2_ARCH = x86_64
 endif
 
