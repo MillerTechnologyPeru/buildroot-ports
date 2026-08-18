c903c1389dc393fde383badf564f293b1e4bc039
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-08-18 09:46:53 -0400

    Point gnome-disk-utility at libelogind
    logind is a combo defaulting to libsystemd:
    
        meson.build:90:15: ERROR: Dependency "libsystemd" not found
    
    The image runs elogind, whose libelogind carries the same sd-login API.
    Select it and name it as a dependency.

--- package/apps/gnome-disk-utility/gnome-disk-utility.mk
+++ package/apps/gnome-disk-utility/gnome-disk-utility.mk
@@ -17,8 +17,14 @@ GNOME_DISK_UTILITY_LICENSE_FILES = COPYING
 #   ERROR: Subproject libhandy is buildable: NO
 #
 # The GTK 4 port is 51, still in beta as of writing.
-GNOME_DISK_UTILITY_DEPENDENCIES = host-pkgconf host-desktop-file-utils libgtk3 libhandy libcanberra libnotify udisks2 libpwquality libsecret libdvdread xz
+GNOME_DISK_UTILITY_DEPENDENCIES = host-pkgconf host-desktop-file-utils libgtk3 libhandy libcanberra libnotify udisks2 libpwquality libsecret libdvdread xz elogind
 
-GNOME_DISK_UTILITY_CONF_OPTS = -Dman=false
+# logind is a combo - libsystemd, libelogind or none - defaulting to
+# libsystemd:
+#
+#   meson.build:90:15: ERROR: Dependency "libsystemd" not found
+#
+# The image runs elogind, whose libelogind carries the same sd-login API.
+GNOME_DISK_UTILITY_CONF_OPTS = -Dman=false -Dlogind=libelogind
 
 $(eval $(meson-package))
