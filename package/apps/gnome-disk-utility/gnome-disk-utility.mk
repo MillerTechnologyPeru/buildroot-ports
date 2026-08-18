bb14fcb9074ca0d65e5f63b71d3a9066ebe38cfb
Alsey Coleman Miller <alseycmiller@gmail.com>
2026-08-18 08:42:42 -0400

    Add libhandy, and give gnome-disk-utility the toolkit it is built on
    Disks 46 is a GTK 3 application on libhandy - meson.build asks for
    gtk+-3.0, libhandy-1, libcanberra-gtk3 and libnotify - but the package
    named libgtk4 and libadwaita, and libhandy was not packaged, so
    configure fell back to a wrap:
    
        ERROR: Subproject libhandy is buildable: NO
    
    The GTK 4 port is 51, still in beta. Add libhandy 1.8.3 (hash from
    GNOME's sha256sum; introspection is a feature, vapi a boolean) and
    correct the dependencies. libcanberra's gtk3 half already comes with
    BR2_PACKAGE_LIBGTK3_X11, and xz is for liblzma.

--- package/apps/gnome-disk-utility/gnome-disk-utility.mk
+++ package/apps/gnome-disk-utility/gnome-disk-utility.mk
@@ -9,7 +9,15 @@ GNOME_DISK_UTILITY_SOURCE = gnome-disk-utility-$(GNOME_DISK_UTILITY_VERSION).tar
 GNOME_DISK_UTILITY_SITE = https://download.gnome.org/sources/gnome-disk-utility/46
 GNOME_DISK_UTILITY_LICENSE = GPL-2.0+
 GNOME_DISK_UTILITY_LICENSE_FILES = COPYING
-GNOME_DISK_UTILITY_DEPENDENCIES = host-pkgconf host-desktop-file-utils libgtk4 libadwaita udisks2 libpwquality libsecret libdvdread
+# Disks 46 is a GTK 3 application on libhandy, not GTK 4 on libadwaita -
+# meson.build asks for gtk+-3.0, libhandy-1, libcanberra-gtk3 and
+# libnotify. The dependencies here named the wrong toolkit, and libhandy
+# was not packaged at all, so configure fell back to a wrap:
+#
+#   ERROR: Subproject libhandy is buildable: NO
+#
+# The GTK 4 port is 51, still in beta as of writing.
+GNOME_DISK_UTILITY_DEPENDENCIES = host-pkgconf host-desktop-file-utils libgtk3 libhandy libcanberra libnotify udisks2 libpwquality libsecret libdvdread xz
 
 GNOME_DISK_UTILITY_CONF_OPTS = -Dman=false
 
---
+++ package/libs/libhandy/libhandy.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# libhandy
+#
+################################################################################
+
+LIBHANDY_VERSION_MAJOR = 1.8
+LIBHANDY_VERSION = $(LIBHANDY_VERSION_MAJOR).3
+LIBHANDY_SOURCE = libhandy-$(LIBHANDY_VERSION).tar.xz
+LIBHANDY_SITE = https://download.gnome.org/sources/libhandy/$(LIBHANDY_VERSION_MAJOR)
+LIBHANDY_LICENSE = LGPL-2.1+
+LIBHANDY_LICENSE_FILES = COPYING
+LIBHANDY_INSTALL_STAGING = YES
+LIBHANDY_DEPENDENCIES = host-pkgconf libgtk3 fribidi
+
+# introspection is a feature (enabled/disabled), vapi a boolean.
+LIBHANDY_CONF_OPTS = \
+	-Dvapi=false \
+	-Dgtk_doc=false \
+	-Dtests=false \
+	-Dexamples=false \
+	-Dglade_catalog=disabled
+
+ifeq ($(BR2_PACKAGE_GOBJECT_INTROSPECTION),y)
+LIBHANDY_CONF_OPTS += -Dintrospection=enabled
+LIBHANDY_DEPENDENCIES += gobject-introspection
+else
+LIBHANDY_CONF_OPTS += -Dintrospection=disabled
+endif
+
+$(eval $(meson-package))
